df0109587a8a26dc8beb38a3937770c8ff3dcc9c
Dominique <dominique.lopez@digitregroup.com>
2019-08-15 17:27:56 +0200

    added random color

--- app-react/src/index.js
+++ app-react/src/index.js
@@ -1,7 +1,56 @@
-import React from 'react';
-import { render } from 'react-dom';
+import React, { Component } from 'react'
+import { render } from 'react-dom'
 
-const node = document.querySelector('#root');
-const element = <div>Hello World!</div>
+const randomColor = () => {
+    const color = '#' + Math.random().toString(16).substr(-6);
+    console.log('color:', color);
+    return  color;
+}
 
-render(element, node);
+class Card extends Component {
+  render() {
+    const style = {
+      padding: 20,
+      textAlign: 'center',
+      color: 'white',
+      backgroundColor: this.props.color,
+    }
+
+    return (
+      <div style={style}>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+class App extends Component {
+
+  state = {
+    color: 'skyblue'
+  }
+
+  randomizeColor = () => this.setState({color: randomColor()})
+
+  render() {
+    const {color} = this.state
+
+    const style = {
+      padding: 20,
+    }
+
+    return (
+      <div style={style}>
+        <Card color={color}>
+          <input
+            type={'button'}
+            value={'Randomize Color: ' + color}
+            onClick={this.randomizeColor}
+          />
+        </Card>
+      </div>
+    )
+  }
+}
+
+render(<App />, document.querySelector('#root'))
